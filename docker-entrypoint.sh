273b99e240b13b016ae5b8a0460c944387f82043
saikurella1 <saikurella1919@gmail.com>
2026-07-14 21:53:28 +0530

    added cache in the docker entry point file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,9 @@ echo "====================================="
 echo "Starting Drupal Container..."
 echo "====================================="
 
+echo "Rebuilding Drupal cache..."
+
+vendor/bin/drush cr -y
+
+echo "Cache rebuild completed."
 exec apache2-foreground
